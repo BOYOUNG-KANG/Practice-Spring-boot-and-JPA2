b4cacce0b29d3c45ae4401a1126b339385c72afd
boyoung <28287@naver.com>
2023-05-16 20:50:57 +0900

    [feat] 회원 조회 api 개발

--- src/main/java/jpabook/real1/api/MemberApiController.java
+++ src/main/java/jpabook/real1/api/MemberApiController.java
@@ -2,21 +2,47 @@ package jpabook.real1.api;
 
 import jpabook.real1.domain.Member;
 import jpabook.real1.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream() //list member를 list memberdto로 변환
+                .map(m->new MemberDto(m.getUsername()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponce saveMemberV1(@RequestBody @Valid Member member) {
@@ -30,6 +56,26 @@ public class MemberApiController {
         member.setUsername(request.getName());
         Long id = memberService.join(member);
         return new CreateMemberResponce(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponce updateMemberV2
+            (@PathVariable("id") Long id,
+             @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findMember(id);
+        return new UpdateMemberResponce(findMember.getUsername(), findMember.getId());
+
+    }
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponce {
+        private String name;
+        private Long id;
     }
 
     @Data
